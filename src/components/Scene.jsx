b098a8b8badc5aa74c7aa696fe95783d602d9290
gurxt <jung_jungian@outlook.com>
2023-07-18 16:21:49 -0400

    first person controls working

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,24 +1,22 @@
-import Tifa from "./Tifa"
+import Tifa from "./models/Tifa"
 import { Stage } from "@react-three/drei"
 import { Perf } from "r3f-perf"
-import { Suspense, useRef } from "react"
+import { Suspense } from "react"
 import FirstPersonControls from "./util/FirstPersonControls"
 
 
 export default function Scene() {
-  const ref = useRef()
-
-  return (
+   return (
     <>
     <Perf position="bottom-left" />
     <Stage
       ContactShadow={{ opacity: 0.2, blur: 3 }}
-      environment="sunset" 
+      environment="night" 
       preset="portrait"
       intensity={2}
     >
       <Suspense fallback={<mesh><boxGeometry /><meshBasicMaterial wireframe/></mesh>}>
-        <Tifa position={[0, -1, 0]} />
+        {/*<Tifa scale={1.5} position={[0, -1, 0]} />*/}
       </Suspense>
     </Stage>
     <gridHelper args={[100, 100]} />
